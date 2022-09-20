a5515d48929534a947acc5dfc8cdae9afa7ec8bb
Geraska <tuftedfun@gmail.com>
2022-09-20 17:26:44 +0600

    learning_process

--- CSharp_learn/Program.cs
+++ CSharp_learn/Program.cs
@@ -4,45 +4,53 @@
     {
         static void Main()
         {
-            //Console.WriteLine(Math.Sqrt(25));
-            Console.WriteLine("Ведите радиус круга: ");
-            double radius = Convert.ToDouble(Console.ReadLine());
-            double area = returnDouble(radius);
-            Console.WriteLine("Площадь круга с радиусом {0} равна {1}", radius, area);
+            //byte[] nums = { 1, 4, 6, 3, 6, 10 };
+            //byte number = countSum(nums);
+            //Console.WriteLine("Result: " + number);  
+            //string word = "Hello!";
 
-            Study student = new Study("name", 29);
-            student.age = 40;
+            //word = String.Concat(word, "!!");
+            //Console.WriteLine(String.Compare(word, "Hello!!"));
+            //string people = "Alex,Bob,John";
+            //string[] names = people.Split(new char[] { ',' });
+            //people = String.Join(" ", names);
+            //Console.WriteLine(people);
+            //Console.WriteLine(word.Substring(0));
+            //Console.WriteLine("Enter a text: ");
+            //string text = Console.ReadLine();
+            //using (FileStream stream = new("info.txt", FileMode.OpenOrCreate))
+            //{
+            //    byte[] array = System.Text.Encoding.Default.GetBytes(text);
+
+            //    stream.Write(array, 0, array.Length);
+            //}
+
+            //using FileStream stream1 = File.OpenRead("info.txt");
+            //{
+            //    byte[] array = new byte[stream1.Length];
+            //    stream1.Read(array, 0, array.Length);
+
+            //    string textFromFile = System.Text.Encoding.Default.GetString(array);
+            //    Console.WriteLine(textFromFile);
+            //}
+
+            try
+            {
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num);
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("You entered wrong format!");
+            }
         }
 
-        static double returnDouble(double radius)
-        {
-            double area = Math.PI * Math.Pow(radius, 2);
-            return area;
-
-        }
-    }
-    class Study
-    {
-        private string name { get; set; }
-
-        public int age { get; set; }
-
-        public Study(string name, int age)
-        {
-            this.name = name;
-            this.age = age;
-        }
-
-        private string privateMethod()
-        {
-
-            return name;
-        }
-
-        public void publicMethod()
-        {
-            Console.WriteLine(privateMethod());
-        }
-        
+        //private static byte countSum(byte[] numbers)
+        //{
+        //    short sum = 0;   
+        //    foreach(byte b in numbers)
+        //        sum += b;
+        //    return Convert.ToByte(sum);
+        //}
     }
 }
